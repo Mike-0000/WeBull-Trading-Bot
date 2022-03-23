fbcbafa63ae5ab5837c6859190d74c12c2497c32
Mike319 <93947725+Mike-0000@users.noreply.github.com>
2022-03-22 21:17:09 -0400

    Working V1.4 - DOGE added

--- main.py
+++ main.py
@@ -6,29 +6,33 @@ import config.userconfig as cfg
 wb = webull()
 # print(wb.get_mfa(cfg.wb_email))
 # print(wb.get_security(cfg.wb_email))
-ASTR_BUY = 4.2
-ASTR_SELLS = [4.5, 4.7]
-#ASTR_SELL2 = 4.7
+
+DOGE_BUY = 0.1185
+DOGE_SELL = 0.124
+ASTR_BUY = 4.15
+ASTR_SELLS = [4.5, 4.75]
+# ASTR_SELL2 = 4.7
 UPDATE_INTERVAL = 45
 ASTR_symbol = 'ASTR'
+DOGE_SYMBOL = 'DOGEUSD'
 
 global SELL_DATA
 
 data = wb.login(cfg.wb_email, cfg.webull_pass, cfg.HOSTNAME, cfg.AUTH_CODE, '1001', cfg.ANSWER)
-                # 6 digits MFA, Security Question ID, Question Answer.
+# 6 digits MFA, Security Question ID, Question Answer.
 
 wb.get_trade_token(cfg.TRADE_TOKEN)
-
-print(data)
 
 counter = 0
 ASTRLevel = 0
 
+
 def getPriceWeight(setPrice, currentPrice):
-    difference = currentPrice/setPrice
+    difference = currentPrice / setPrice
     percentage = 1 - difference
     if percentage < 0:
         percentage = -percentage
+    return percentage
 
 
 def getPositions(ticker):
@@ -59,9 +63,19 @@ def getBid(ticker):
             max1 = float(price["price"])
     return float(max1)
 
+def getCryptoAsk(ticker):
+    ask = getCryptoBidsAsks(ticker, "ask")
+    return float(ask)
+
+
+def getCryptoBid(ticker):
+    bid = getCryptoBidsAsks(ticker, "bid")
+    return float(bid)
+
 
 def getBidsAsks(ticker, mode):
     quote = wb.get_quote(ticker)
+    #print(quote)
     askbidlist = quote['depth']
     asks = askbidlist['ntvAggAskList']
     bids = askbidlist['ntvAggBidList']
@@ -70,79 +84,130 @@ def getBidsAsks(ticker, mode):
     else:
         return asks
 
+def getCryptoBidsAsks(ticker, mode):
+        quote = wb.get_quote(ticker)
+        asks = quote["askList"][0]['price']
+        bids = quote["bidList"][0]['price']
+        if mode == 'bid':
+            return bids
+        else:
+            return asks
 
-def getPrice(ticker):
-    stock = getAnalysis(ticker)
-    return stock['targetPrice']['current']
+def placeCryptoOrder(ticker, num, ask, mode):
+    d = wb.place_order_crypto(stock=ticker, action=mode, orderType='MKT', quant=num, enforce="IOC", entrust_type='QTY')
+    print(mode + ": " + str(num) + " for $" + str(ask))
+    print(d)
+    return d
 
 
 def placeOrder(ticker, num, ask, mode):
-    d = wb.place_order(stock=ticker, price=ask, action=mode, orderType='LMT', enforce='GTC',
-                   quant=num)
+    d = wb.place_order(stock=ticker, price=ask, action=mode, orderType='LMT', enforce='GTC', quant=num)
     print(mode + ": " + str(num) + " for $" + str(ask))
     return d
+
 
 while 0 < 1:
     try:
         now = datetime.now()
         current_time = now.hour
 
-        if current_time >= 7 and current_time <= 21:  # Is Market Open??
+        ###  Crypto Logic
 
+        numOfDOGE = getPositions(DOGE_SYMBOL)
+        DOGEbid = getCryptoBid(DOGE_SYMBOL)  ### DOGE
+        DOGEask = getCryptoAsk(DOGE_SYMBOL)
+
+        sell_doge = getPriceWeight(DOGE_SELL, DOGEbid)  ### DOGE
+        buy_doge = getPriceWeight(DOGE_BUY, DOGEask)
+
+        if counter % UPDATE_INTERVAL == 0:
+            print("Buy Percentage: " + str(buy_doge*100) + " Sell Percentage: " + str(sell_doge*100))
+            print("DOGE Bid: " + str(DOGEbid) + " Ask: " + str(DOGEask))
+            print("DOGE Position: " + str(numOfDOGE))
+            print("Buy at: " + str(DOGE_BUY) + " Sell At: " + str(DOGE_SELL))
+
+        if counter % 20 == 0:
+            wb.refresh_login()
+            time.sleep(1)
+
+        num = 1.1 / DOGEbid  # Calculate number of shares to equal 1.1 dollars
+
+
+        if sell_doge < 0.0075:
+            if numOfDOGE * DOGEbid < 1:
+                continue
+            placeCryptoOrder(DOGE_SYMBOL, int(num), DOGEbid, "SELL")
+            time.sleep(20000 * sell_doge)
+            continue
+
+        if buy_doge < 0.0075:
+            placeCryptoOrder(DOGE_SYMBOL, int(num), DOGEask, "BUY")
+            time.sleep(80000 * buy_doge)
+            continue
+
+
+
+
+
+        #  US Stock Market
+
+        if current_time >= 7 and current_time <= 19:  # Is Market Open??
+
+            ### Collect position information
             numOfASTR = getPositions(ASTR_symbol)
-            if numOfASTR < 160 and numOfASTR > 70 and ASTRLevel != 1:
+
+            if numOfASTR >= 120 and ASTRLevel != 0:
+                ASTRLevel = 0
+                print("Setting level to 0")
+            if numOfASTR < 120 and numOfASTR > 40 and ASTRLevel != 1:
                 ASTRLevel = 1
                 print("Setting level to 1")
-            if numOfASTR <= 70 and ASTRLevel != 2:
+            if numOfASTR <= 40 and ASTRLevel != 2:
                 ASTRLevel = 2
                 print("Setting level to 2")
-            ASTRprice = getPrice(ASTR_symbol)  # Get price from analysis
 
-            ASTRask = getAsk(ASTR_symbol)
+            # Get current price from analysis
 
+            # Get current bid and ask
+
+            ASTRask = getAsk(ASTR_symbol)  ### ASTR
             ASTRbid = getBid(ASTR_symbol)
 
             ###   ASTR LOGIC
-                    ### Checking If Order Filled Logic
-            if ASTRLevel == 1 and numOfASTR > 150: ### Check if Order is filled
-                time.sleep(60)    ### If not, allow time for order to be filled
-                if numOfASTR > 150:    ### If Order still not filled, cancel order and reset level.
+            ### Checking If Order Filled Logic
+            if ASTRLevel == 1 and numOfASTR > 150:  ### Check if Order is filled
+                time.sleep(60)  ### If not, allow time for order to be filled
+                if numOfASTR > 150:  ### If Order still not filled, cancel order and reset level.
                     print(wb.cancel_all_orders())
                     ASTRLevel = 0
                     continue
 
-
             if counter % UPDATE_INTERVAL == 0:
-                print("Bid: " + str(ASTRbid) + " Ask: " + str(ASTRask))
+                print("ASTR Bid: " + str(ASTRbid) + " Ask: " + str(ASTRask))
                 print("Level: " + str(ASTRLevel) + " Position: " + str(numOfASTR))
                 print("Buy at: " + str(ASTR_BUY) + " Sell At: " + str(ASTR_SELLS[ASTRLevel]))
 
+            sell_astr = getPriceWeight(ASTR_SELLS[ASTRLevel], ASTRbid)  ### ASTR
+            buy_astr = getPriceWeight(ASTR_BUY, ASTRask)
 
+            if sell_astr < 0.01:
+                placeOrder(ASTR_symbol, 1, ASTRbid, "SELL")
+                time.sleep(1000 * sell_astr)  # print("SELL 1 @ " + str(ASTRbid))
+
+            if buy_astr < 0.01:
+                placeOrder(ASTR_symbol, 1, ASTRask, "BUY")
+                time.sleep(1000 * buy_astr)  # print("SELL 1 @ " + str(ASTRask))
 
             ### BUYING ASTR
 
-            if ASTRask <= ASTR_BUY and ASTRLevel >= 1:  # BUYING
-                placeOrder(ASTR_symbol, int(getPositions(ASTR_symbol)+5), ASTRask+0.02, "BUY")
-                ASTRLevel = ASTRLevel - 1
-
-            ### SELLING ASTR
-
-            if ASTRbid >= ASTR_SELLS[0] and ASTRLevel == 0:  # SELLING
-                SELL_DATA = placeOrder(ASTR_symbol, int(getPositions(ASTR_symbol)/2), ASTRbid-0.02, "SELL")
-                ASTRLevel = ASTRLevel + 1
-
-            if ASTRbid >= ASTR_SELLS[1] and ASTRLevel == 1:  # SELLING Level 2
-                SELL_DATA2 = placeOrder(ASTR_symbol, int(getPositions(ASTR_symbol)/2), ASTRbid-0.02, "SELL")
-                ASTRLevel = ASTRLevel + 1
+            # if ASTRask <= ASTR_BUY and ASTRLevel >= 1:  # BUYING  #     placeOrder(ASTR_symbol, int(getPositions(ASTR_symbol)+5), ASTRask+0.02, "BUY")  #     ASTRLevel = ASTRLevel - 1  #  # ### SELLING ASTR  #  # if ASTRbid >= ASTR_SELLS[0] and ASTRLevel == 0:  # SELLING  #     SELL_DATA = placeOrder(ASTR_symbol, int(getPositions(ASTR_symbol)/2), ASTRbid-0.02, "SELL")  #     ASTRLevel = ASTRLevel + 1  #  # if ASTRbid >= ASTR_SELLS[1] and ASTRLevel == 1:  # SELLING Level 2  #     SELL_DATA2 = placeOrder(ASTR_symbol, int(getPositions(ASTR_symbol)/2), ASTRbid-0.02, "SELL")  #     ASTRLevel = ASTRLevel + 1
 
 
 
         else:
             # print(current_time)
-            time.sleep(180)
-            print("Market Closed")
             counter = counter + 1
-            if counter % 2 == 0:
+            if counter % 100 == 0:
                 print(wb.refresh_login())
 
         ###  Misc Logic
